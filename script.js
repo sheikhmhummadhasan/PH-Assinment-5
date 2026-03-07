708c4963ddf5cd35e348eb40023f98b5c4b02711
sheikhmhummadhasan <hasan2001onlin@gmail.com>
2026-03-07 15:56:59 +0600

    add toggel btn

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let cardsDiv = document.querySelector(".cards");
 let isshuCard_api = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
 
-let cardshow =async () =>{
+
+let cardshow = async () => {
     let url = await fetch(isshuCard_api);
     let res = await url.json();
-    console.log(res.data)
+
+    document.querySelector(".manage-info h3").innerText = res.data.length;
+
     res.data.forEach((e) => {
         let creatAcard = document.createElement("div");
         creatAcard.innerHTML = `            <div class="card">
@@ -19,7 +22,7 @@ let cardshow =async () =>{
                     <p>${e.description}</p>
                     <div class="bugs"> 
                         <span class="bug bug-1"><i class="ri-bug-fill"></i> ${e.labels[0]}</span>
-                        <span class="bug bug-2"><i class="ri-file-info-fill"></i> ${e.labels[1]}</span>
+                        <span class="bug bug-2"><i class="ri-file-info-fill"></i> ${e.labels[1] ? e.labels[1] : "error"}</span>
                     </div>
                 </div>
                 <!-- s-3 -->
@@ -28,7 +31,44 @@ let cardshow =async () =>{
                     <p class="">${e.createdAt}</p>
                 </div>
             </div>`
-            cardsDiv.appendChild(creatAcard)
+        cardsDiv.appendChild(creatAcard)
+        // console.log(e)
     })
 }
-cardshow()
+
+// toggoling btn
+let tgl_btns = document.querySelectorAll(".b")
+tgl_btns.forEach((t) => {
+    t.addEventListener("click", () => {
+        tgl_btns.forEach((btn) => {
+            btn.classList.remove("toggel")
+            btn.classList.add("btn-2")
+        })
+        console.log(t)
+        t.classList.remove("btn-2")
+        t.classList.add("toggel")
+    })
+    
+})
+
+// login interface
+function loginInterface() {
+    let main = document.querySelector(".main")
+    let login = document.querySelector(".login-interface");
+    let input_username = document.querySelector("#username");
+    let input_password = document.querySelector("#password");
+    let login_btn = document.querySelector(".primery-btn");
+
+    login_btn.addEventListener("click", () => {
+        let user = input_username.value;
+        let pass = input_password.value;
+        // console.log(user,pass)
+        if (user === "admin" && pass === "admin123") {
+            login.classList.add("hidden");
+            main.classList.remove("hidden")
+        } else {
+            alert("Wrong username or password")
+        }
+    })
+}
+loginInterface()
